8ef14d027901bb0ee16cf4d77301dfd1ad983a86
Lance Bierke <lancebierke@gmail.com>
2021-06-22 22:34:22 -0700

    Functional Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,15 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import {
   Card,
   CardImg,
-  //CardImgOverlay,
   CardText,
   CardBody,
   CardTitle,
 } from "reactstrap";
 
-class CampsiteInfo extends Component {
-  renderCampsite(campsite) {
+
+  function RenderCampsite({campsite}) {
     return (
       <div className="col-md-5 m-1">
         <Card>
@@ -23,15 +22,15 @@ class CampsiteInfo extends Component {
     );
   }
 
-  renderComments(comments) {
-    if (comments)
+  function RenderComments({comments}) {
+    if (comments) {
       return (
         <div className="col-md-5 m-1">
           <h4>Comments</h4>
           {comments.map((comment) => {
             return (
-              <p>
-                {comment.text} <br />
+                <div key={comment.id}>
+                    <p>{comment.text}<br />
                 -- {comment.author},{" "}
                 {new Intl.DateTimeFormat("en-US", {
                   year: "numeric",
@@ -39,23 +38,27 @@ class CampsiteInfo extends Component {
                   day: "2-digit",
                 }).format(new Date(Date.parse(comment.date)))}
               </p>
+        </div>
             );
           })}
         </div>
       );
   }
-  render() {
-    if (this.props.campsite) {
+  return <div />;
+}
+
+  function CampsiteInfo(props) {
+    if (props.campsite) {
       return (
         <div className="container">
           <div className="row">
-            {this.renderCampsite(this.props.campsite)}
-            {this.renderComments(this.props.campsite.comments)}
+            <RenderCampsite campsite={props.campsite} />
+            <RenderComments comments={props.campsite.comments} />
           </div>
         </div>
       );
     }
     return <div />;
   }
-}
+  
 export default CampsiteInfo;
